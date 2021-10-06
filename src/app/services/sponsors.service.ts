39f4c72b9559cf55d2afb7eb37a5d9c0cdebcd35
Rosa Estevan Hernaiz <rosaestevanhernaiz@gmail.com>
2021-10-06 09:28:19 +0200

    Retoques finales reunión cliente.

--- src/app/services/sponsors.service.ts
+++ src/app/services/sponsors.service.ts
@@ -8,7 +8,7 @@ export class SponsorsService {
   baseUrl: string;
 
   constructor(private httpClient: HttpClient) {
-    this.baseUrl = 'http://2530-79-157-110-6.ngrok.io/api/sponsors/';
+    this.baseUrl = 'http://localhost:3000/api/sponsors/';
   }
 
   getSponsor(): Promise<any> {
--- src/app/services/users-services.service.ts
+++ src/app/services/users-services.service.ts
@@ -9,7 +9,7 @@ export class UsersServicesService {
 
   // arrUsers: User[];
   constructor(private httpClient: HttpClient) {
-    this.baseUrl = 'http://2530-79-157-110-6.ngrok.io/api/users/';
+    this.baseUrl = 'http://localhost:3000/api/users/';
     //   this.arrUsers = [
     //     {
     //       id: 1,
